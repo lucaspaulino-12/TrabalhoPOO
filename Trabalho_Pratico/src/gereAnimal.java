5865470eba5c649f944e4b931ca47635cd89c291
lucaspaulino-12 <lucasepaulino12@gmail.com>
2025-06-02 17:55:06 +0100

    Initial commit

---
+++ Trabalho_Pratico/src/gereAnimal.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class gereAnimal {
+	ArrayList<Animal> animal;
+	
+	gereAnimal(){
+		animal = new ArrayList<Animal>();
+	}
+	
+	public void mostrarAnimais() {
+		
+	}
+}
